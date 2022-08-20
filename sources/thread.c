f7a823fb338e156cd39008ba8a9a1c3ffdf054bf
Adeline GOUET <agouet@e1r12p19.clusters.42paris.fr>
2022-08-20 14:44:27 +0200

    last debug

--- sources/thread.c
+++ sources/thread.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   thread.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lollith <lollith@student.42.fr>            +#+  +:+       +#+        */
+/*   By: agouet <agouet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 16:31:24 by agouet            #+#    #+#             */
-/*   Updated: 2022/08/19 23:04:49 by lollith          ###   ########.fr       */
+/*   Updated: 2022/08/20 14:42:51 by agouet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,11 @@ int	check_fork_eat(int *pt_left, int *pt_right, int *pt_num, t_philo *philo)
 
 	while (!one_die && all_eat != 0)
 	{
+		if (philo->rules->nb_philo % 2 != 0)
+		{
 		while (philo->t_take_fork > 1 && get_time() - philo->rules->time_ini < 2 * philo->rules->t_eat + 10 + philo->t_take_fork)
-			usleep (50);
+			usleep (2000);
+		}
 		if (left < right)
 		{
 			pthread_mutex_lock(&philo->rules->m_fork[left]);
@@ -139,7 +142,7 @@ void	*routine_philo(void *arg)
 	if (num % 2 == 0)
 		usleep(philo->rules->t_eat * 1000);
 	while (!one_die && philo->rules->all_eat != 0)
-	{	
+	{
 		if (check_fork_eat(&num, &right, &num, philo) == 0)
 			return (NULL);
 		if (sleeping(&num, philo->rules, philo) == 0)
@@ -153,55 +156,4 @@ void	*routine_philo(void *arg)
 	return (NULL);
 }
 
-
-void	*reaper(void *arg)
-{
-	t_philo	*temp;
-	t_philo	*philo;
-
-	philo = (t_philo *)arg;
-
-	long begin_eat;
-	long state_diff;
-	long actual_state;
-	while (1)
-	{
-		temp = philo;
-		while (temp && temp->num < temp->rules->nb_philo)
-		{
-			if (!check_die_meal(temp))
-				return (NULL);
-			pthread_mutex_lock(&temp->m_state);
-			begin_eat = temp->begin_eat;
-			pthread_mutex_unlock(&temp->m_state);
-			actual_state = get_time() - philo->rules->time_ini;
-			state_diff = actual_state - begin_eat;
-			if (actual_state > begin_eat &&  state_diff > temp->rules->t_die )
-			{
-				pthread_mutex_lock(&philo->rules->m_one_die);
-				philo->rules->one_die = 1;
-				pthread_mutex_unlock(&philo->rules->m_one_die);
-				dying(&temp->num, temp->rules, temp);
-				return (NULL);
-			}
-		temp = temp->next;
-
-		}
-		usleep(8 * 1000);
-	}
-	return (NULL);
-}
-
-int	check_die_meal(t_philo *philo)
-{
-	int	one_die;
-	int	all_eat;
-	pthread_mutex_lock(&philo->rules->m_one_die);
-	one_die = philo->rules->one_die;
-	all_eat = philo->rules->all_eat;
-	pthread_mutex_unlock(&philo->rules->m_one_die);
-	if (one_die || all_eat == 0)
-		return (FAILURE);
-	return (SUCCESS);
-}
 
